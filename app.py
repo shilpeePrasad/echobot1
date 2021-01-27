2d9d834fcdefd084390f6436ffed644155af08e2
PrasadSV <Shilpee.Prasad@unisys.com>
2021-01-27 19:18:13 +0530

    cpoy herocard code

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 from flask import Flask,request,Response
+from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings,TurnContext,ConversationState,MemoryStorage
 from botbuilder.schema import Activity
-from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings
 import asyncio
+from herocard import SampleAnimationCard
 
-from echobot import EchoBot
 
-app=Flask(__name__)
-loop=asyncio.get_event_loop()
+app = Flask(__name__)
+loop = asyncio.get_event_loop()
 
-botadaptersettings = BotFrameworkAdapterSettings("","")
-botadapter = BotFrameworkAdapter(botadaptersettings)
+botsettings = BotFrameworkAdapterSettings("487908b7-f461-4c20-bf8d-19796717a765","Rw-.U3EYQMz~Nxjnd58qO4Nn2u4_k1X61D")
+botadapter = BotFrameworkAdapter(botsettings)
 
-ebot = EchoBot()
+CONMEMORY = ConversationState(MemoryStorage())
+botdialog = SampleAnimationCard()
+
 
 @app.route("/api/messages",methods=["POST"])
 def messages():
     if "application/json" in request.headers["content-type"]:
-      jsonmessage = request.json
+        body = request.json
     else:
-      return Response(status=415)
+        return Response(status = 415)
 
-    activity=Activity().deserialize(jsonmessage)
+    activity = Activity().deserialize(body)
 
-    async def turn_call(turn_context):
-        await ebot.on_turn(turn_context)
+    auth_header = (request.headers["Authorization"] if "Authorization" in request.headers else "")
 
-    task=loop.create_task(botadapter.process_activity(activity,"",turn_call))
+    async def call_fun(turncontext):
+        await botdialog.on_turn(turncontext)
+
+    task = loop.create_task(
+        botadapter.process_activity(activity,auth_header,call_fun)
+        )
     loop.run_until_complete(task)
+
 
 if __name__ == '__main__':
     app.run('localhost',3978)
